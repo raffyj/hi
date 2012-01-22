8370d72d98b89995dcc561fc0227eccc64f0bd07
unknown <Saturn@Saturnpc.(none)>
2012-01-22 15:03:08 +0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hi::Application.config.secret_token = '69fe8a382ed5cc8972c90a967c4547b68512290031b17d46d3955964043dce0f61a20a6275ab758a5cb78f51a92b8747e253e30fb7f4d0ef05406a88eca87cea'
